b4b5cfafaf61a95d288830089cf9901fb0c8cca8
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-04-27 16:46:13 +0300

    ...

---
+++ finalTask/src/main/java/com/example/finalTask/dto/HotelListResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.finalTask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotelListResponseDto {
+    private List<HotelResponseDto> hotels;
+    private Long total;
+}
---
+++ finalTask/src/main/java/com/example/finalTask/dto/HotelResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.finalTask.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotelResponseDto {
+    private Long id;
+    private String name;
+    private String advertisementTitle;
+    private String city;
+    private String address;
+    private Double distanceFromCityCenter;
+    private Double rating;
+    private Integer numberOfRatings;
+}
