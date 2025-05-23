2e36075d11156d3883ad62190cb105c28f8768e1
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-05-23 17:01:29 +0300

    ...

--- finalTask/src/main/java/com/example/finalTask/configuration/SecurityConfig.java
+++ finalTask/src/main/java/com/example/finalTask/configuration/SecurityConfig.java
@@ -1,14 +1,52 @@
 package com.example.finalTask.configuration;
 
+import com.example.finalTask.utils.UserRole;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/users/register").permitAll()
+                        .requestMatchers("/api/v1/hotels/**").hasAnyRole(UserRole.ROLE_ADMIN.name(), UserRole.ROLE_USER.name())
+                        .requestMatchers("/api/v1/rooms/**").hasAnyRole(UserRole.ROLE_ADMIN.name(), UserRole.ROLE_USER.name())
+                        .requestMatchers("/api/v1/bookings/**").hasAnyRole(UserRole.ROLE_ADMIN.name(), UserRole.ROLE_USER.name())
+                        .requestMatchers("/api/v1/admin/**").hasRole(UserRole.ROLE_ADMIN.name())
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .httpBasic(basic -> basic.realmName("Hotel Booking API"));
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
